c624df5e2f554d64153e1d1d90c3cc3bcbb807bc
noel-fr <fnoel6050@gmail.com>
2025-12-10 20:35:14 -0600

    Model y Controllers

---
+++ SegudoExamen/Models/Libro.cs
@@ -0,0 +1,26 @@
+﻿using Google.Cloud.Firestore;
+
+namespace SegundoExamen.Models
+{
+    [FirestoreData]
+    public class Libro
+    {
+        [FirestoreProperty]
+        public string Id { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public string Titulo { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public string Autor { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public string ISBN { get; set; } = string.Empty; // Debe ser único
+
+        [FirestoreProperty]
+        public string Categoria { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public string Editorial { get; set; } = string.Empty;
+
+        [FirestoreProperty] public int AnoPublicacion { get; set; } = int.MaxValue;
---
+++ SegudoExamen/Models/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using Google.Cloud.Firestore;
+
+namespace SegundoExamen.Models
+{
+    [FirestoreData]
+    public class Usuario
+    {
+        [FirestoreProperty]
+        public string Id { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public string Nombre { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public string Apellido { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public string Correo { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public string NumeroIdentidad { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public int Edad { get; set; }
+
+        [FirestoreProperty]
+        public string Telefono { get; set; } = string.Empty;
+
+        [FirestoreProperty]
+        public string Rol { get; set; } = "usuario";
+
+        [FirestoreProperty]
+        public bool Activo { get; set; } = true;
+
+        [FirestoreProperty]
+        public DateTime FechaRegistro { get; set; } = DateTime.UtcNow;
+
+        [FirestoreProperty]
+        public decimal Multas { get; set; } = 0;
+    }
+}
